c3cdec749dcd76c357a56f9d4662647d23943ef8
Marcus Broberg <marcus.broberg@talentech.com>
2024-08-15 11:10:06 +0200

    Handles JSON body

--- ExampleFromMicrosoft/HttpExample.cs
+++ ExampleFromMicrosoft/HttpExample.cs
@@ -1,10 +1,17 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Extensions.Abstractions;
 using Microsoft.Extensions.Logging;
+using System.Text.Json;
 
 namespace My.Functions
 {
+    public class Person
+    {
+        public string? firstname { get; set; }
+        public string? lastname { get; set; }
+    }
     public class HttpExample
     {
         private readonly ILogger<HttpExample> _logger;
@@ -15,10 +22,20 @@ namespace My.Functions
         }
 
         [Function("HttpExample")]
-        public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
+        public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
         {
-            _logger.LogInformation("C# HTTP trigger function processed a request.");
-            return new OkObjectResult("Welcome to Azure Functions!");
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            //dynamic data = JsonConvert.DeserializeObject(requestBody);
+
+            //var body = new StreamReader(req.Body).ReadToEnd();
+            Person? person= JsonSerializer.Deserialize<Person>(requestBody);
+           var query = req.Query;
+           var name = query.First(c => c.Key == "name").Value;
+           if (person is null || person.firstname is null || person.lastname is null){
+            return new OkObjectResult($"Not a valid person!");
+           }
+            //_logger.LogInformation("C# HTTP trigger function processed a request.");
+            return new OkObjectResult($"Welcome {person.firstname} {person.lastname}!");
         }
     }
 }
